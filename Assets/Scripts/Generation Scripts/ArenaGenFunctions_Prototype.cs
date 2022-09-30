ed52286c974d44e2528d9e21d38b1dbcdc70cd5c
Isaac Taylor <isaac.taylor@ontariotechu.net>
2022-09-30 12:17:44 -0400

    Generation Scripts from Proto

---
+++ Assets/Scripts/Generation Scripts/ArenaGenFunctions_Prototype.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Generation_Tools
+{
+    public static class ArenaGenFunctions_Prototype { 
+        public static void ArenaPrototype(Space2D arena)
+        {
+            BasicBuilderFunctions.Flood(arena, new Cell(0), new Cell(1), 1, 1, 29, 29);
+			MakeRiver(arena);
+			MakeClusters(arena);
+			MakeSpacedOut(arena, new Cell(1));
+        }
+
+        public static void MakeRiver(Space2D arena)
+        {
+            List<Coord> riverPoints = new List<Coord> { };
+            Coord curPoint = new Coord();
+
+			for (int i = 0; i < 5; i++)
+			{
+				bool unique = false;
+
+				while (!unique)
+				{
+					bool yes = true;
+					curPoint = (RNG.GenRandCoord(arena));
+					for (int j = 0; j < i; j++)
+					{
+						if (curPoint.x == riverPoints[j].x && curPoint.y == riverPoints[j].y)
+						{
+							yes = false;
+						}
+					}
+					if (yes)
+					{
+						unique = yes;
+						riverPoints.Add(curPoint);
+					}
+
+				}
+
+			}
+
+			BasicBuilderFunctions.Connect3(arena, riverPoints[0], riverPoints[1], riverPoints[2], new Cell(2));
+			BasicBuilderFunctions.Connect3(arena, riverPoints[2], riverPoints[3], riverPoints[4], new Cell(2));
+		}
+		public static void MakeClusters(Space2D arena)
+		{
+			Cell clusterValue = new Cell(3);
+
+			for (int i = 0; i < (RNG.GenRand(1, 3)); i++)
+			{
+				Space2D selection = new Space2D(RNG.GenRand(3, 8), RNG.GenRand(3, 8));
+				BasicBuilderFunctions.Cluster(selection, clusterValue);
+				selection.worldOrigin = RNG.GenRandCoord(new Space2D(arena.width - (selection.width + 1), arena.height - (selection.height + 1)), true);
+
+				BasicBuilderFunctions.CopySpaceAToB(selection, arena, new List<Cell>{ clusterValue});
+			}
+		}
+		public static void MakeSpacedOut(Space2D arena, Cell validSpace)
+        {
+			int keepGoing = 30;
+			for (int i = 0; i < RNG.GenRand(3, 5); i++)
+			{
+				bool valid = false;
+				Coord surrounding;
+
+				while (!valid && keepGoing > 0)
+				{
+					Coord pos = RNG.GenRandCoord(arena);
+
+					if (arena.GetCell(pos) == validSpace.value)
+					{
+						surrounding = BasicBuilderFunctions.CheckAdjacentCells(arena, pos, true, validSpace);
+						if (surrounding.x == 0 && surrounding.y == 0)
+						{
+							surrounding = BasicBuilderFunctions.CheckDiagonalCells(arena, pos, true, validSpace);
+							if (surrounding.x == 0 && surrounding.y == 0)
+							{
+								arena.SetCell(pos, new Cell(4));
+								valid = true;
+							}
+						}
+					}
+
+					if (!valid)
+					{
+						keepGoing--;
+					}
+				}
+
+			}
+
+			if(keepGoing == 0)
+            {
+				UnityEngine.Debug.Log("damn, that's a bit cringe");
+            }
+		}
+	
+    }
+
+}
---
+++ Assets/Scripts/Generation Scripts/MakeArenaArray.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Assets.Scripts.Generation_Tools;
+using UnityEngine.UI;
+using TMPro;
+
+public class MakeArenaArray : MonoBehaviour
+{
+    public Space2D arena;
+    public TextMeshProUGUI iAmLessSad;
+    public List<GameObject> prefabs = new List<GameObject> { };
+    public Transform boardOrigin;
+    public bool OhNo = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        arena = new Space2D(30, 30);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Return))
+        {
+            if (OhNo)
+            {
+                ArenaGenFunctions_Prototype.ArenaPrototype(arena);
+               // PrintToTextMesh();
+                DisplaySpace2D();
+            }
+            else
+            {
+                Space2D myEntireAss = new Space2D(28, 28);
+                BasicBuilderFunctions.Flood(myEntireAss, new Cell(0), new Cell(1));
+                myEntireAss.worldOrigin = new Coord(1, 1);
+                BasicBuilderFunctions.CopySpaceAToB(myEntireAss, arena, new List<Cell> { });
+             //   PrintToTextMesh();
+                DisplaySpace2D();
+            }
+            OhNo = !OhNo;
+        }
+    }
+
+    protected void PrintToTextMesh()
+    {
+        iAmLessSad.text = "\n";
+        for(int i = 0; i < arena.height; i++)
+        {
+            for(int j = 0; j < arena.width; j++)
+            {
+                iAmLessSad.text =  iAmLessSad.text + (arena.GetCell(new Coord(j, i))) + " ";
+            }
+            iAmLessSad.text = iAmLessSad.text + "\n";
+        }
+    }
+
+    protected void DisplaySpace2D()
+    {
+
+        Infanticide(boardOrigin.transform);
+        
+        for (int i = 0; i < arena.height; i++)
+        {
+            for (int j = 0; j < arena.width; j++)
+            {
+
+                GameObject currentTile = Instantiate(prefabs[arena.GetCell(new Coord(j,i))], new Vector3(boardOrigin.position.x + j, 0, boardOrigin.position.z - i), Quaternion.identity);
+                currentTile.transform.SetParent(boardOrigin);
+                //iAmLessSad.text = iAmLessSad.text + (arena.GetCell(new Coord(j, i))) + " ";
+
+            }
+            iAmLessSad.text = iAmLessSad.text + "\n";
+        }
+    }
+
+    protected void Infanticide(Transform parent)
+    {
+        foreach (Transform child in parent)
+        {
+            GameObject.Destroy(child.gameObject);
+        }
+    }
+}
