d1ef89c92b1bff941cc990d4368f6d890882dd5c
HarrisonLeswick <72412866+HarrisonLeswick@users.noreply.github.com>
2023-03-27 17:20:14 -0400

    Camera Spins Both Sky and Follow
    ⠀  ⠀   (\__/)
               (•ㅅ•)      The key to swap
        ＿ノヽ ノ＼＿      directions is H.
    `/　`/ ⌒Ｙ⌒ Ｙ  ヽ
    ( 　(三ヽ人　 /　  |
    |　ﾉ⌒＼ ￣￣ヽ   ノ
    ヽ＿＿＿＞､＿_／
           ｜( 王 ﾉ〈     (\__/)
           /ﾐ`ー―彡\    (•ㅅ•)     OK
          / ╰    ╯ \ /    \>

--- Assets/Scripts/Camera scrpt/Follow.cs
+++ Assets/Scripts/Camera scrpt/Follow.cs
@@ -5,17 +5,38 @@ using Cinemachine;
 
 public class Follow : MonoBehaviour
 {
+    private GameObject player;
+    private GameObject otherPlayer;
+    private GameObject skyCam;
+    public static bool isOther;
 
     // Start is called before the first frame update
     void Start()
     {
-        gameObject.GetComponent<CinemachineVirtualCamera>().LookAt = GameObject.FindWithTag("Player").transform;
-        gameObject.GetComponent<CinemachineVirtualCamera>().Follow = GameObject.FindWithTag("Player").transform;
+        player = GameObject.FindWithTag("Player");
+        otherPlayer = GameObject.FindWithTag("OtherPlayer");
+        skyCam = GameObject.Find("Sky camera");
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       if(Input.GetKeyDown(KeyCode.H)){
+        isOther = !isOther;
+       }
+
+       if(!isOther){
+            gameObject.GetComponent<CinemachineVirtualCamera>().GetCinemachineComponent<CinemachineTransposer>().m_FollowOffset = new Vector3(0,15,-13);
+            gameObject.GetComponent<CinemachineVirtualCamera>().LookAt = player.transform;
+            gameObject.GetComponent<CinemachineVirtualCamera>().Follow = player.transform;
+            skyCam.transform.rotation = Quaternion.Euler(90, 0, 0); 
+       }
+       else if(isOther){
+            gameObject.GetComponent<CinemachineVirtualCamera>().GetCinemachineComponent<CinemachineTransposer>().m_FollowOffset = new Vector3(0,15,13);
+            gameObject.GetComponent<CinemachineVirtualCamera>().LookAt = otherPlayer.transform;
+            gameObject.GetComponent<CinemachineVirtualCamera>().Follow = otherPlayer.transform;
+            skyCam.transform.rotation = Quaternion.Euler(90, 180, 0); 
+       }
     }
 }
